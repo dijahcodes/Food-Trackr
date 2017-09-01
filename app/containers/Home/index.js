54ea14435abd85ef31f99fa740511fb35d79c45e
dijahmac <mcdonald.khadijah@gmail.com>
2017-09-01 16:45:35 -0400

    added button and styling

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -15,8 +15,11 @@ export default class Home extends React.PureComponent {
     return (
       <div className="container">
         <Helmet title="Home" meta={[ { name: 'description', content: 'Description of Home' }]}/>
-
-        //Remove this line and you can start writing your code here.
+        <div className="welcomed">
+          <h1 className="welcome">Welcome to Food Trakr!</h1>
+          <p> A website to track what exactly youre eating.</p>
+          <input type="button" className="button" value="Sign-up"/>
+        </div>
       </div>
     );
   }
