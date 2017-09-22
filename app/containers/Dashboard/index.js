d76434808e696a5ad2c673dc25884841e73ceb92
dijahmac <mcdonald.khadijah@gmail.com>
2017-09-22 11:13:13 -0400

    added code for each log

--- app/containers/Dashboard/index.js
+++ app/containers/Dashboard/index.js
@@ -6,25 +6,61 @@
 
 import React from 'react';
 import Helmet from 'react-helmet';
+import {Link} from 'react-router';
 
 import './style.css';
 import './styleM.css';
 
 export default class Dashboard extends React.PureComponent {
+  constructor()
+  {
+    super();
+    this.state = {
+      food:'',
+      calories:'',
+      fat:'',
+      logs:[]
+    }
+  }
+
+showLogs = () => {
+  fetch('http://localhost:8000/api/getLogss',{
+    method: 'GET'
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    this.setState({
+      logs:json.logs
+    })
+  }.bind(this))
+}
+
+componentWillMount() {
+  this.showLogs();
+}
+
+
   render() {
     return (
       <div className="container">
         <Helmet title="Dashboard" meta={[ { name: 'description', content: 'Description of Dashboard' }]}/>
 
             <input type="submit" value="sign-out" className="sign-out"/>
-            <h2 className="food-welcome">Food Trakr</h2>
+              <h2 className="food-welcome">Food Trakr</h2>
+              <Link to={'/Newlog'} className="add-new-dash">Add new</Link>
         <div className="dashboard-container">
-          <input type="submit" value="Add New" className="add-new-dash"/>
-            <div className="logs">
 
+            <div className="logs">
+            {this.state.logs.map((log, index) => (
+              <div className='log'><p>food: {log.food} ... fat: {log.fat} ... calories: {log.calories}</p></div>
+            ))}
             </div>
+
+
         </div>
-        
+
       </div>
     );
   }
--- app/containers/Newlog/index.js
+++ app/containers/Newlog/index.js
@@ -11,6 +11,78 @@ import './style.css';
 import './styleM.css';
 
 export default class Newlog extends React.PureComponent {
+  constructor()
+  {
+    super();
+    this.state = {
+      food:'',
+      calories:'',
+      fat:'',
+      carbs:'',
+      sugars:'',
+      log:[]
+    }
+  }
+
+  handleFood = (event) =>
+  {
+    this.setState({
+      food:event.target.value
+    })
+  }
+
+  handleCalories = (event) =>
+  {
+    this.setState({
+      calories:event.target.value
+    })
+  }
+
+  handleFat = (event) =>
+  {
+    this.setState({
+      fat:event.target.value
+    })
+  }
+
+  handleCarbs = (event) =>
+  {
+    this.setState({
+      carbs:event.target.value
+    })
+  }
+
+  handleSugar = (event) =>
+  {
+    this.setState({
+      sugars:event.target.value
+    })
+  }
+  
+
+  storeFood = () =>
+  {
+    let data = new FormData();
+    data.append('calories', this.state.calories);
+    data.append('food', this.state.food);
+    data.append('fat', this.state.fat);
+    data.append('carbs', this.state.carbs);
+    data.append('sugar', this.state.sugar);
+
+    fetch('http://localhost:8000/api/storeLog', {
+      method: 'POST',
+      body: data
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      this.setState({
+        log:json.log
+      })
+    }.bind(this))
+  }
+
   render() {
     return (
       <div className="container">
@@ -21,14 +93,14 @@ export default class Newlog extends React.PureComponent {
           <h1>Create a food log.</h1>
           <div className="date-input"><span>Date</span><input type="text" value="Date" className="date"/></div>
 
-          <div className="box-input"><span>Food </span><input type="text" value="food" className="food" label="Food"/></div>
+          <div className="box-input"><span>Food </span><input type="text" value={this.state.food} className="food" label="Food" onChange={this.handleFood}/></div>
 
           <div className="input-boxes">
 
-            <div className="box-input"><span>Calories </span><input type="text" value="calories" className="calories"/></div>
-            <div className="box-input"><span>Fat </span><input type="text" value="fat" className="fat"/></div>
-            <div className="box-input"><span>Carbs </span><input type="text" value="Carbs" className="carbs" /></div>
-            <div className="box-input"><span>Sugars </span><input type="text" value="Sugars" className="sugars"/></div>
+            <div className="box-input"><span>Calories </span><input type="text" value={this.state.calories} className="calories" onChange={this.handleCalories}/></div>
+            <div className="box-input"><span>Fat </span><input type="text" value={this.state.fat} className="fat" obChange={this.handleFat}/></div>
+            <div className="box-input"><span>Carbs </span><input type="text" value={this.state.carbs} className="carbs" onChange={this.handleCarbs}/></div>
+            <div className="box-input"><span>Sugars </span><input type="text" value={this.state.sugars} className="sugars" onChange={this.handleSugar}/></div>
           </div>
 
         <div className="allergens">
@@ -41,7 +113,7 @@ export default class Newlog extends React.PureComponent {
             <span>Shellfish</span><input type="checkbox" value="Shellfish" className="shellfish" />
             <span>Fish</span><input type="checkbox" value="Fish" className="fish"/>
         </div>
-            <input type="submit" value="Add" className="add-new" />
+            <button type="button" value="Add" onClick={this.storeFood()} className="add-new">Add</button>
             <input type="submit" value="Cancel" className="cancel-new" />
             </div>
 
